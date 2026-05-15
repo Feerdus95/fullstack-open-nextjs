6ee21b4f20cacf352da6a8693ca376149ac13f14
Feerdus <fmartinez@comparen.net>
2026-05-15 19:43:09 -0300

    fix: reduce homepage padding from py-16/py-24 to py-8/py-12

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Homepage from "./homepage.mdx"
 
 export default function Home() {
   return (
-    <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-16 sm:py-24">
+    <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8 sm:py-12">
       <div className="markdown">
         <Homepage />
       </div>
